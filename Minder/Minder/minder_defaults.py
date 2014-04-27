340fb7a188bb82c623191567b9795e000d24a61e
harlanaubuchon <harlanaubuchon@gmail.com>
2014-04-27 00:50:17 -0700

    Fixed Windows bugs, updated defaults
    Fixed windows bugs, added file_size_limit to defaults

--- Minder/Minder/minder_defaults.py
+++ Minder/Minder/minder_defaults.py
@@ -5,6 +5,7 @@ DEFAULT_CONFIG = {
                      "defaults": {
                          "space_remaining_threshold_bytes": 2048,
                          "text_difference_threshold_percentage": 99,
+                         "file_size_limit_in_kilobytes": 300000,
                          },
                      "minds": {}
                      }
--- Minder/Minder/minds.py
+++ Minder/Minder/minds.py
@@ -8,12 +8,14 @@ import hashlib
 import json
 import mimetypes
 from functools import reduce
+import minder_config as mc
 
 
-MIND_DIR = '/.mind'
-MIND = '/mind_palace.json'
-MIND_FILE = MIND_DIR + MIND
-
+MIND_DIR = '.mind'
+MIND = 'mind_palace.json'
+MIND_FILE = os.path.join(MIND_DIR, MIND)
+USER_DIR = os.path.expanduser('~')
+M_CONFIG = mc.minderconfig()
 
 def mind(mind_path, refresh=False):
     """ 
@@ -23,7 +25,7 @@ def mind(mind_path, refresh=False):
     directory.
     """
 
-    file_path = mind_path + MIND_FILE  # Replace this with Class attribute
+    file_path = os.path.join(mind_path, MIND_FILE)
     mind_time = datetime.fromtimestamp(time.time()).isoformat()[:23] + 'Z'
     mind_dir = {mind_path: {"minded_datetime": mind_time}}
 
@@ -34,8 +36,8 @@ def mind(mind_path, refresh=False):
         else:
             mind_palace = _read_mind_palace(mind_path)
     else:
-        if os.path.isdir(mind_path + MIND_DIR) is False:
-            os.makedirs(mind_path + MIND_DIR)
+        if os.path.isdir(os.path.join(mind_path, MIND_DIR)) is False:
+            os.makedirs(os.path.join(mind_path, MIND_DIR))
         mind_palace = interrogate(mind_path, mind_dir)
         _write_mind_palace(mind_path, mind_palace)
 
@@ -52,16 +54,32 @@ def interrogate(mind_path, mind_dir):
     file_dir = {}
     mind_path = mind_path.rstrip(os.sep)
     start = mind_path.rfind(os.sep) + 1
+    size_limit = int(M_CONFIG['defaults']['file_size_limit_in_kilobytes'])
+    
     for path, dirs, files in os.walk(mind_path):
         folders = path[start:].split(os.sep)
         subdir = dict.fromkeys(files)
         if len(files) > 0:
             for file_name in files:
-                with open(path + '/' + file_name) as file_handle:
-                    file_data = file_handle.read()
-                    checksum = hashlib.md5(file_data).hexdigest()
-                    mime_type = mimetypes.guess_type(file_name)[0].split('/')
-                    subdir[file_name] = {"md5_sum": checksum, "mime_type": mime_type}
+                file_path = os.path.join(path, file_name)
+                file_size = int(os.path.getsize(file_path) / 1024)
+                if file_size < size_limit:
+                    print "%s - %s < %s - %s" % (type(file_size), file_size, type(size_limit), size_limit)
+                    with open(file_path) as file_handle:
+                        file_data = file_handle.read()
+                        checksum = hashlib.md5(file_data).hexdigest()
+                else:
+                    checksum = None
+                guess_mime = mimetypes.guess_type(file_name)
+
+                if guess_mime[0] is not None:
+                    mime_type = guess_mime[0].split('/')
+                else:
+                    mime_type = None
+                    #print guess_mime
+                    
+                subdir[file_name] = {"md5_sum": checksum, "mime_type": mime_type, "file_size": file_size}
+
         parent = reduce(dict.get, folders[:-1], file_dir)
         parent[folders[-1]] = subdir
 
@@ -73,7 +91,7 @@ def interrogate(mind_path, mind_dir):
 
 def _write_mind_palace(mind_path, mind_palace):
     je = json.JSONEncoder(indent=4, sort_keys=True, encoding="utf-8")
-    file_path = mind_path + MIND_FILE  # Replace this with Class attribute
+    file_path = os.path.join(mind_path, MIND_FILE)
     with open(file_path, 'w') as write_handle:
         json_dir = je.encode(mind_palace)
         write_handle.write(json_dir)
@@ -81,7 +99,7 @@ def _write_mind_palace(mind_path, mind_palace):
 
 def _read_mind_palace(mind_path):
     jd = json.JSONDecoder(encoding="utf-8")
-    file_path = mind_path + MIND_FILE  # Replace this with Class attribute
+    file_path = os.path.join(mind_path, MIND_FILE)
     with open(file_path) as read_handle:
         json_string = read_handle.read()
         json_dir = jd.decode(json_string)
